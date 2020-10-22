16ce47b4716b665d75a2936547e6792d37214cc0
yogeshsingh <yogeshsingh101200@gmail.com>
2020-10-22 18:56:47 +0530

    added models

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    followers_count = models.PositiveIntegerField(blank=True, default=0)
+    following_count = models.PositiveIntegerField(blank=True, default=0)
+
+
+class Post(models.Model):
+    content = models.CharField(max_length=150)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    like_count = models.PositiveIntegerField(blank=True, default=0)
+
+
+class Like(models.Model):
+    by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="likes")
+    on = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="likes")
+
+
+class Connection(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    follows = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="followers")
