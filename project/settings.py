aa9be3ff8ae175926be505991c4574eea9f0e593
yogeshsingh <yogeshsingh101200@gmail.com>
2020-10-23 11:26:42 +0530

    setuped frontend

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backend',
-    'rest_framework'
+    'rest_framework',
+    'frontend'
 ]
 
 MIDDLEWARE = [
