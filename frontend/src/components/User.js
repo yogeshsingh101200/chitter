d4c2d695b3712d479b1898055523a3121b7c5968
yogeshsingh <yogeshsingh101200@gmail.com>
2020-11-18 12:48:04 +0530

    added show followers and following

--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -2,6 +2,37 @@ import React, { Component } from "react";
 import Card from "react-bootstrap/Card";
 import Follow from "./Follow";
 import axios from "axios";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import ListGroup from "react-bootstrap/ListGroup";
+import { Link } from "react-router-dom";
+
+function ModalList(props) {
+    const listItems = props.list.map(item => (
+        <ListGroup.Item key={item} as={Link} className="text-decoration-none" to={`/user/${item}`}>
+            {item}
+        </ListGroup.Item>
+    ));
+
+    return (
+        <Modal
+            show={props.show}
+            onHide={props.handleClose}
+            centered
+            size="sm"
+        >
+            <Modal.Body>
+                <ListGroup>
+                    {listItems}
+                </ListGroup>
+            </Modal.Body>
+            <Button variant="success" onClick={props.handleClose}>
+                Close
+            </Button>
+        </Modal>
+    );
+}
+
 
 export class User extends Component {
 
@@ -9,7 +40,11 @@ export class User extends Component {
         super(props);
 
         this.state = {
-            user: null
+            user: null,
+            following: [],
+            followers: [],
+            showFollowing: false,
+            showFollowers: false
         };
     }
 
@@ -28,6 +63,32 @@ export class User extends Component {
             .catch(err => {
                 console.log(err.response.status);
             });
+
+        axios
+            .get("/api/following", config)
+            .then(res => {
+                const list = [];
+                res.data.forEach(record => {
+                    list.push(record.follows);
+                });
+                this.setState({ following: list });
+            })
+            .catch(err => {
+                console.log(err.response.status);
+            });
+
+        axios
+            .get("/api/followers", config)
+            .then(res => {
+                const list = [];
+                res.data.forEach(record => {
+                    list.push(record.user);
+                });
+                this.setState({ followers: list });
+            })
+            .catch(err => {
+                console.log(err.response.status);
+            });
     };
 
     componentDidMount() {
@@ -37,29 +98,45 @@ export class User extends Component {
     render() {
         if (this.state.user) {
             return (
-                <Card className="profile">
-                    <Card.Body>
-                        <div className="custom-card-header d-flex justify-content-between align-items-center mb-2">
-                            <Card.Title className="custom-card-title my-auto">@{this.state.user.username}</Card.Title>
-                            {
-                                this.props.isAuthenticated && this.state.user.username !== this.props.user.username ?
-                                    <Follow
-                                        connections={this.state.user.followers}
-                                        authenticatedUser={this.props.user.id}
-                                        user={this.state.user.id}
-                                        refresh={this.refresh}
-                                    />
-                                    : ""
-                            }
-                        </div>
-                        <Card.Link href="#">
-                            <span className="font-weight-bold stats">{this.state.user.following_count}</span> Following
-                    </Card.Link>
-                        <Card.Link href="#">
-                            <span className="font-weight-bold stats">{this.state.user.followers_count}</span> Followers
-                    </Card.Link>
-                    </Card.Body>
-                </Card>
+                <>
+                    <Card className="profile">
+                        <Card.Body>
+                            <div className="custom-card-header d-flex justify-content-between align-items-center mb-2">
+                                <Card.Title className="custom-card-title my-auto">@{this.state.user.username}</Card.Title>
+                                {
+                                    this.props.isAuthenticated && this.state.user.username !== this.props.user.username ?
+                                        <Follow
+                                            connections={this.state.user.followers}
+                                            authenticatedUser={this.props.user.id}
+                                            user={this.state.user.id}
+                                            refresh={this.refresh}
+                                        />
+                                        : ""
+                                }
+                            </div>
+                            <Card.Link
+                                onClick={this.state.user.following_count > 0 ? () => this.setState({ showFollowing: true }) : void (0)}
+                            >
+                                <span className="font-weight-bold stats">{this.state.user.following_count}</span> Following
+                            </Card.Link>
+                            <Card.Link
+                                onClick={this.state.user.followers_count > 0 ? () => this.setState({ showFollowers: true }) : void (0)}
+                            >
+                                <span className="font-weight-bold stats">{this.state.user.followers_count}</span> Followers
+                            </Card.Link>
+                        </Card.Body>
+                    </Card>
+                    <ModalList
+                        show={this.state.showFollowing}
+                        list={this.state.following}
+                        handleClose={() => this.setState({ showFollowing: false })}
+                    />
+                    <ModalList
+                        show={this.state.showFollowers}
+                        list={this.state.followers}
+                        handleClose={() => this.setState({ showFollowers: false })}
+                    />
+                </>
             );
         } else {
             return (
