02394c32224618aca2f794db76e8fdb4ba29e3f7
yogeshsingh <yogeshsingh101200@gmail.com>
2020-11-20 22:33:49 +0530

    added form validation in login form

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,24 +3,18 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import axios from "axios";
 import { Redirect } from "react-router-dom";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+const schema = Yup.object({
+    username: Yup.string()
+        .required("Required"),
+    password: Yup.string()
+        .required("Required")
+});
 
 export class Login extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: "",
-            password: ""
-        };
-    }
-
-    handleChange = e => {
-        this.setState({ [e.target.name]: e.target.value });
-    };
-
-    handleSubmit = e => {
-        e.preventDefault();
-
+    handleSubmit = (values, actions) => {
         const config = {
             headers: {
                 "Accept": "application/json",
@@ -28,9 +22,7 @@ export class Login extends Component {
             }
         };
 
-        const username = this.state.username;
-        const password = this.state.password;
-        const body = JSON.stringify({ username, password });
+        const body = JSON.stringify(values);
 
         axios
             .post("/api/auth/login", body, config)
@@ -38,8 +30,14 @@ export class Login extends Component {
                 localStorage.setItem("token", res.data.token);
                 this.props.authentication("LOGIN", res.data.user);
             })
-            .catch(err => {
-                console.log("err.res.data=", err.response.data, "err.res.status=", err.response.status);
+            .catch(exception => {
+                if (exception.response && exception.response.status === 400) {
+                    actions.setFieldError("general", exception.response.data.non_field_errors[0]);
+                } else {
+                    console.log("exception", exception);
+                    console.log("exception.response", exception.response);
+                }
+                actions.setSubmitting(false);
             });
     };
 
@@ -49,37 +47,65 @@ export class Login extends Component {
         } else {
             return (
                 <div className="form-container">
-                    <Form className="form" onSubmit={this.handleSubmit} noValidate>
-                        <h3 className="mb-3 font-weight-normal text-center">Login</h3>
-                        <Form.Group controlId="username">
-                            <Form.Label>Username</Form.Label>
-                            <Form.Control
-                                type="text"
-                                name="username"
-                                value={this.state.username}
-                                onChange={this.handleChange}
-                                placeholder="Enter username"
-                                autoFocus
-                                autoComplete="off"
-                            />
-                        </Form.Group>
+                    <Formik
+                        initialValues={{ username: "", password: "" }}
+                        validationSchema={schema}
+                        onSubmit={this.handleSubmit}
+                    >
+                        {formik => (
+                            <Form className="form" onSubmit={formik.handleSubmit} noValidate>
+                                <h3 className="mb-3 font-weight-normal text-center">Login</h3>
+                                {
+                                    formik.errors.general && (
+                                        <div className="text-center form-level-errors">
+                                            {formik.errors.general}
+                                        </div>
+                                    )
+                                }
+                                <Form.Group controlId="username">
+                                    <Form.Label>Username</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        name="username"
+                                        placeholder="Enter username"
+                                        isValid={formik.touched.username && !formik.errors.username}
+                                        isInvalid={formik.touched.username && formik.errors.username}
+                                        autoComplete="off"
+                                        {...formik.getFieldProps("username")}
+                                    />
+                                    <Form.Control.Feedback type="valid">
+                                        Looks Good!
+                                    </Form.Control.Feedback>
+                                    <Form.Control.Feedback type="invalid">
+                                        {formik.errors.username}
+                                    </Form.Control.Feedback>
+                                </Form.Group>
 
-                        <Form.Group controlId="password">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control
-                                type="password"
-                                name="password"
-                                value={this.state.password}
-                                onChange={this.handleChange}
-                                placeholder="Enter password"
-                            />
-                        </Form.Group>
+                                <Form.Group controlId="password">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control
+                                        type="password"
+                                        name="password"
+                                        placeholder="Enter password"
+                                        isValid={formik.touched.password && !formik.errors.password}
+                                        isInvalid={formik.touched.password && formik.errors.password}
+                                        {...formik.getFieldProps("password")}
+                                    />
+                                    <Form.Control.Feedback type="valid">
+                                        Looks Good!
+                                    </Form.Control.Feedback>
+                                    <Form.Control.Feedback type="invalid">
+                                        {formik.errors.password}
+                                    </Form.Control.Feedback>
+                                </Form.Group>
 
-                        <Button variant="primary" type="submit" block>
-                            Login
-                    </Button>
-                    </Form>
-                </div>
+                                <Button variant="primary" type="submit" block disabled={formik.isSubmitting}>
+                                    Login
+                                </Button>
+                            </Form>
+                        )}
+                    </Formik>
+                </div >
             );
         }
     }
